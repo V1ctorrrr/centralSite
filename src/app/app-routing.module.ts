909e473113855492a2200b074cf0ca74a94d1aef
Victor De Decker <s129144@ap.be>
2023-06-23 16:37:48 +0200

    add darkmode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { Portfolio3dComponent } from './portfolio3d/portfolio3d.component';
+import { GithubComponent } from './github/github.component';
 
 const routes: Routes = [];
 
@@ -8,7 +11,10 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot([
     { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'portfolio3D', component: Portfolio3dComponent },
+  { path: 'github', component: GithubComponent },
   ],
   { useHash: true })],
   exports: [RouterModule]
